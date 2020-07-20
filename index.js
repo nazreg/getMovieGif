62f13dfd33d8e7ef314feb039d10d096afe0c233
Marcelo Nazario <ind7@protonmail.com>
2020-07-20 16:44:31 -0500

    add index.js server to obscure APIKEY

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+const PORT = process.env.PORT !== undefined ? process.env.PORT : 3000;
+
+app.get("/movies", (req, res) => {
+  const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_APIKEY}&s=${req.query.s}`;
+  axios.get(url).then((movies) => {
+    return res.json(movies.data);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
